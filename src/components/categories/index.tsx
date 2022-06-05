22cdc1228cb0de955478b5dcbc27aee90f40f703
Mansur <vitrigovm@gmail.com>
2022-06-05 12:27:40 +0300

    logic for changing categories, refactor structure

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
+const categories = [
+  "Все",
+  "Мясные",
+  "Вегетарианская",
+  "Гриль",
+  "Острые",
+  "Закрытые",
+];
 
 const Categories = () => {
+  const [activeCategory, setActiveCategory] = useState<number>(0);
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((item, index) => (
+          <li
+            className={activeCategory === index ? "active" : ""}
+            onClick={() => setActiveCategory(index)}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </div>
   );
