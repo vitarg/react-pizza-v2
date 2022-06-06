59b49e8e533ec273f4d2ce857c394499a8cff4c0
Mansur <vitrigovm@gmail.com>
2022-06-06 21:20:21 +0300

    receiving pizzas is now a request for a back

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./header";
 import Categories from "./categories";
 import Sort from "./sort";
 import PizzaBlock from "./pizza-block";
 import "../scss/app.scss";
-import pizzas from "../assets/pizzas.json";
+
+interface Pizza {
+  id: number;
+  title: string;
+  types: number[];
+  sizes: number[];
+  imageUrl: string;
+  price: number;
+  category: number;
+  rating: number;
+}
 
 const App = () => {
+  const [pizzas, setPizzas] = useState<Pizza[]>([]);
+
+  useEffect(() => {
+    fetch("https://619116e741928b001768ff38.mockapi.io/pizzas").then(
+      (response) => response.json().then(setPizzas)
+    );
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
